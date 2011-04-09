fe2d5afeb6478b43e86211d7beacc4e8ac812d9c
Paŭlo Ebermann <paul@fencing-game.de>
2011-04-09 23:10:09 +0200

    package-info: explanation of "Usually not ..."

--- src/com/jcraft/jsch/package-info.java
+++ src/com/jcraft/jsch/package-info.java
@@ -11,5 +11,14 @@
  *  <dd>A connection to a SSH server. Used to configure settings,
  *     port forwardings and to open channels.</dd>
  * </dt>
+ *
+ * <p>The string <em>Usually not to be used by applications.</em>
+ *   at the beginning of a class or interface description means
+ *   that this type is used internally by the library, and normally
+ *   there is no need for an application to directly use
+ *   (or implement/extend) this type.  It may be needed if you need to
+ *   extend functionality, then you normally will also have to change some
+ *   {@linkplain com.jcraft.jsch.JSch#setConfig configuration options}.
+ * </p>
  */
 package com.jcraft.jsch;
