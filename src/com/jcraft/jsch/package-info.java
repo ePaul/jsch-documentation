86c22963b64e6866c40d78ebe04c690a0d1df43c
Paŭlo Ebermann <paul@fencing-game.de>
2011-04-12 18:22:26 +0200

    some more text for package-info.java.

--- src/com/jcraft/jsch/package-info.java
+++ src/com/jcraft/jsch/package-info.java
@@ -10,7 +10,24 @@
  *  <dt>{@link com.jcraft.jsch.Session}</dt>
  *  <dd>A connection to a SSH server. Used to configure settings,
  *     port forwardings and to open channels.</dd>
+ *  <dt>{@link com.jcraft.jsch.Channel}</dt>
+ *  <dd>and its subclasses {@link com.jcraft.jsch.ChannelExec},
+ *     {@link com.jcraft.jsch.ChannelShell},
+ *     {@link com.jcraft.jsch.ChannelSubsystem} for remote command
+ *      execution.</dd>
+ *  <dt>{@link com.jcraft.jsch.ChannelSftp}</dt>
+ *  <dd>for secure file transfer. You might then also need
+ *      {@link com.jcraft.jsch.SftpATTRS} and implement
+ *      {@link com.jcraft.jsch.SftpProgressMonitor}.</dd>
+ *  <dt>{@link com.jcraft.jsch.UserInfo} and
+ *      {@link com.jcraft.jsch.UIKeyboardInteractive}</dt>
+ *  <dd> should be implemented to allow feedback to the user and/or
+ *     password or keyboard-interactive authentication.</dd>
  * </dt>
+ * <p>
+ *   Most other classes are either for internal use (see next paragraph)
+ *   or for special uses, and not necessary for a normal application.
+ * </p>
  *
  * <p>The string <em>Usually not to be used by applications.</em>
  *   at the beginning of a class or interface description means
