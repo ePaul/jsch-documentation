d2ebe77d9618d6e513d48be23ff540813bce0ff4
Paŭlo Ebermann <paul@fencing-game.de>
2011-05-01 22:01:39 +0200

    Exec: cleanup, comments.

--- examples/Exec.java
+++ examples/Exec.java
@@ -4,82 +4,72 @@ import java.awt.*;
 import javax.swing.*;
 import java.io.*;
 
+/**
+ * This example demonstrates execution of a remote command.
+ *<p>
+ *  You will be asked username, hostname, passwd and command.
+ *  If everything works fine, given command will be invoked 
+ *  on the remote side and outputs will be printed out.
+ *</p>
+ */
 public class Exec{
-  public static void main(String[] arg){
-    try{
-      JSch jsch=new JSch();  
+  public static void main(String[] arg)
+    throws Exception
+  {
 
-      String host=null;
-      if(arg.length>0){
-        host=arg[0];
-      }
-      else{
-        host=JOptionPane.showInputDialog("Enter username@hostname",
-                                         System.getProperty("user.name")+
-                                         "@localhost"); 
-      }
-      String user=host.substring(0, host.indexOf('@'));
-      host=host.substring(host.indexOf('@')+1);
+    JSch jsch=new JSch();  
 
-      Session session=jsch.getSession(user, host, 22);
+    String host=null;
+    if(arg.length>0){
+      host=arg[0];
+    }
+    else{
+      host=JOptionPane.showInputDialog("Enter username@hostname",
+                                       System.getProperty("user.name")+
+                                       "@localhost"); 
+    }
+    String user=host.substring(0, host.indexOf('@'));
+    host=host.substring(host.indexOf('@')+1);
+
+    Session session=jsch.getSession(user, host, 22);
       
-      /*
-      String xhost="127.0.0.1";
-      int xport=0;
-      String display=JOptionPane.showInputDialog("Enter display name", 
-                                                 xhost+":"+xport);
-      xhost=display.substring(0, display.indexOf(':'));
-      xport=Integer.parseInt(display.substring(display.indexOf(':')+1));
-      session.setX11Host(xhost);
-      session.setX11Port(xport+6000);
-      */
+    // username and password will be given via UserInfo interface.
+    UserInfo ui=new SwingDialogUserInfo();
+    session.setUserInfo(ui);
+    session.connect();
 
-      // username and password will be given via UserInfo interface.
-      UserInfo ui=new SwingDialogUserInfo();
-      session.setUserInfo(ui);
-      session.connect();
+    String command=JOptionPane.showInputDialog("Enter command", 
+                                               "set|grep SSH");
 
-      String command=JOptionPane.showInputDialog("Enter command", 
-                                                 "set|grep SSH");
+    Channel channel=session.openChannel("exec");
+    ((ChannelExec)channel).setCommand(command);
 
-      Channel channel=session.openChannel("exec");
-      ((ChannelExec)channel).setCommand(command);
 
-      // X Forwarding
-      // channel.setXForwarding(true);
+    // no input
+    channel.setInputStream(null);
 
-      //channel.setInputStream(System.in);
-      channel.setInputStream(null);
+    ((ChannelExec)channel).setErrStream(System.err);
 
-      //channel.setOutputStream(System.out);
+    InputStream in=channel.getInputStream();
 
-      //FileOutputStream fos=new FileOutputStream("/tmp/stderr");
-      //((ChannelExec)channel).setErrStream(fos);
-      ((ChannelExec)channel).setErrStream(System.err);
+    channel.connect();
 
-      InputStream in=channel.getInputStream();
-
-      channel.connect();
-
-      byte[] tmp=new byte[1024];
-      while(true){
-        while(in.available()>0){
-          int i=in.read(tmp, 0, 1024);
-          if(i<0)break;
-          System.out.print(new String(tmp, 0, i));
-        }
-        if(channel.isClosed()){
-          System.out.println("exit-status: "+channel.getExitStatus());
-          break;
-        }
-        try{Thread.sleep(1000);}catch(Exception ee){}
+    // loop to read the output until the command finished.
+    byte[] tmp=new byte[1024];
+    while(true){
+      while(in.available()>0){
+        int i=in.read(tmp, 0, 1024);
+        if(i<0)break;
+        System.out.print(new String(tmp, 0, i));
       }
-      channel.disconnect();
-      session.disconnect();
+      if(channel.isClosed()){
+        System.out.println("exit-status: "+channel.getExitStatus());
+        break;
+      }
+      try{Thread.sleep(1000);}catch(Exception ee){}
     }
-    catch(Exception e){
-      System.out.println(e);
-    }
+    channel.disconnect();
+    session.disconnect();
   }
 
 }
--- examples/ViaSSH.java
+++ examples/ViaSSH.java
@@ -89,8 +89,6 @@ public class ViaSSH {
     return useThis.split("@");
   }
 
-
-
 }
 
 
