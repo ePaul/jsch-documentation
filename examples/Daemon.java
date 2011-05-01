9b92ef2e54e9be88f485d311bb6174fadba0f367
Paŭlo Ebermann <paul@fencing-game.de>
2011-05-01 23:50:28 +0200

    Daemon: cleanup, documentation.

--- examples/Daemon.java
+++ examples/Daemon.java
@@ -4,59 +4,82 @@ import java.io.*;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ *  This program will demonstrate how to provide a network service like
+ *  inetd by using remote port-forwarding functionality.
+ * 
+ *  You will be asked username@hostname, port, and passwd.
+ *  If everything works fine, you will see a message about the
+ *  forwarding in stdout.
+ * 
+ *  Try the port on the remote host - the parrot will repeat everything
+ *  you send. (You can quit by sending a message starting with ".", or
+ *  simply by closing the stream from your side.)
+ */
 public class Daemon{
-  public static void main(String[] arg){
+  public static void main(String[] arg)
+    throws Exception
+  {
 
     int rport;
 
-    try{
-      JSch jsch=new JSch();
+    JSch jsch=new JSch();
 
-      String host=null;
-      if(arg.length>0){
-        host=arg[0];
-      }
-      else{
-        host=JOptionPane.showInputDialog("Enter username@hostname",
-                                         System.getProperty("user.name")+
-                                         "@localhost"); 
-      }
-      String user=host.substring(0, host.indexOf('@'));
-      host=host.substring(host.indexOf('@')+1);
-
-      Session session=jsch.getSession(user, host, 22);
-
-      String foo=JOptionPane.showInputDialog("Enter remote port number", 
-                                             "8888");
-      rport=Integer.parseInt(foo);
-
-      // username and password will be given via UserInfo interface.
-      UserInfo ui=new SwingDialogUserInfo();
-      session.setUserInfo(ui);
-
-      session.connect();
-
-      //session.setPortForwardingR(rport, Parrot.class.getName());
-      session.setPortForwardingR(rport, "Daemon$Parrot");
-      System.out.println(host+":"+rport+" <--> "+"Parrot");
+    String host=null;
+    if(arg.length>0){
+      host=arg[0];
     }
-    catch(Exception e){
-      System.out.println(e);
+    else{
+      host=JOptionPane.showInputDialog("Enter username@hostname",
+                                       System.getProperty("user.name")+
+                                       "@localhost"); 
     }
+    String user=host.substring(0, host.indexOf('@'));
+    host=host.substring(host.indexOf('@')+1);
+
+    Session session=jsch.getSession(user, host, 22);
+
+    String foo=JOptionPane.showInputDialog("Enter remote port number", 
+                                           "8888");
+    rport=Integer.parseInt(foo);
+
+    // username and password will be given via UserInfo interface.
+    UserInfo ui=new SwingDialogUserInfo();
+    session.setUserInfo(ui);
+
+    session.connect();
+
+    //session.setPortForwardingR(rport, Parrot.class.getName());
+    session.setPortForwardingR(rport, "Daemon$Parrot");
+    System.out.println(host+":"+rport+" <--> "+"Parrot");
   }
 
+  /**
+   * A very simple implementation of ForwardedTCPIPDaemon, the
+   * Parrot simply repeats what the other side sends. It quits
+   * after the first chunk read which started with ".".
+   */
   public static class Parrot implements ForwardedTCPIPDaemon{
     ChannelForwardedTCPIP channel;
-    Object[] arg;
     InputStream in;
     OutputStream out;
 
-    public void setChannel(ChannelForwardedTCPIP c, InputStream in, OutputStream out){
+    /**
+     * supplies the channel and streams.
+     */
+    public void setChannel(ChannelForwardedTCPIP c,
+                           InputStream in, OutputStream out) {
       this.channel=c;
       this.in=in;
       this.out=out;
     }
-    public void setArg(Object[] arg){this.arg=arg;}
+
+    /** we ignore any arguments. */
+    public void setArg(Object[] arg){ /* ignored. */}
+
+    /**
+     * runs the daemon.
+     */
     public void run(){
       try{
         byte[] buf=new byte[1024];
