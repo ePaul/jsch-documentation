d8122d600a1699052614e08b39b32c9ad92ca074
Paŭlo Ebermann <paul@fencing-game.de>
2011-05-08 19:13:30 +0200

    ViaHTTP: cleanup, comments.

--- examples/ViaHTTP.java
+++ examples/ViaHTTP.java
@@ -1,57 +1,58 @@
 /* -*-mode:java; c-basic-offset:2; indent-tabs-mode:nil -*- */
 import com.jcraft.jsch.*;
-import java.awt.*;
 import javax.swing.*;
 
+/**
+ * This program will demonstrate the ssh session via HTTP proxy.
+ *
+ * You will be asked username, hostname, proxy-server and passwd. 
+ * If everything works fine, you will get the shell prompt.
+ */
 public class ViaHTTP{
-  public static void main(String[] arg){
+  public static void main(String[] arg)
+    throws Exception
+  {
 
     String proxy_host;
     int proxy_port;
 
-    try{
-      JSch jsch=new JSch();
+    JSch jsch=new JSch();
 
-      String host=null;
-      if(arg.length>0){
-        host=arg[0];
-      }
-      else{
-        host=JOptionPane.showInputDialog("Enter username@hostname",
-                                         System.getProperty("user.name")+
-                                         "@localhost"); 
-      }
-      String user=host.substring(0, host.indexOf('@'));
-      host=host.substring(host.indexOf('@')+1);
-
-      Session session=jsch.getSession(user, host, 22);
-
-      String proxy=JOptionPane.showInputDialog("Enter proxy server",
-                                                 "hostname:port");
-      proxy_host=proxy.substring(0, proxy.indexOf(':'));
-      proxy_port=Integer.parseInt(proxy.substring(proxy.indexOf(':')+1));
-
-      session.setProxy(new ProxyHTTP(proxy_host, proxy_port));
-
-      // username and password will be given via UserInfo interface.
-      UserInfo ui=new SwingDialogUserInfo();
-      session.setUserInfo(ui);
-
-      session.connect();
-
-      Channel channel=session.openChannel("shell");
-
-      channel.setInputStream(System.in);
-      channel.setOutputStream(System.out);
-
-      channel.connect();
+    String host=null;
+    if(arg.length>0){
+      host=arg[0];
     }
-    catch(Exception e){
-      System.out.println(e);
+    else{
+      host=JOptionPane.showInputDialog("Enter username@hostname",
+                                       System.getProperty("user.name")+
+                                       "@localhost"); 
     }
+    String user=host.substring(0, host.indexOf('@'));
+    host=host.substring(host.indexOf('@')+1);
+
+    Session session=jsch.getSession(user, host, 22);
+
+    // setup of proxy
+
+    String proxy=JOptionPane.showInputDialog("Enter proxy server",
+                                             "hostname:port");
+    proxy_host=proxy.substring(0, proxy.indexOf(':'));
+    proxy_port=Integer.parseInt(proxy.substring(proxy.indexOf(':')+1));
+
+    session.setProxy(new ProxyHTTP(proxy_host, proxy_port));
+
+    // password will be given via UserInfo interface.
+    UserInfo ui=new SwingDialogUserInfo();
+    session.setUserInfo(ui);
+
+    session.connect();
+
+    Channel channel=session.openChannel("shell");
+
+    channel.setInputStream(System.in);
+    channel.setOutputStream(System.out);
+
+    channel.connect();
   }
 
-
 }
-
-
