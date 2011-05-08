ab17e9685fe0d505bfc5be097cb27ca341a85323
Paŭlo Ebermann <paul@fencing-game.de>
2011-05-08 17:59:33 +0200

    Logger: cleanup, comments.

--- examples/Logger.java
+++ examples/Logger.java
@@ -1,47 +1,54 @@
 /* -*-mode:java; c-basic-offset:2; indent-tabs-mode:nil -*- */
 import com.jcraft.jsch.*;
-import java.awt.*;
 import javax.swing.*;
 
+/**
+ * This program will demonstrate how to enable the logging mechanism and
+ * get logging messages.
+ *
+ * You will be asked username, hostname and passwd.
+ * If everything works fine, you will get the shell prompt
+ * on standard output, and the logging messages on standard error.
+ */
 public class Logger{
-  public static void main(String[] arg){
+  public static void main(String[] arg)
+    throws Exception
+  {
     
-    try{
-      JSch.setLogger(new MyLogger());
-      JSch jsch=new JSch();
+    JSch.setLogger(new MyLogger());
+    JSch jsch=new JSch();
 
-      String host=null;
-      if(arg.length>0){
-        host=arg[0];
-      }
-      else{
-        host=JOptionPane.showInputDialog("Enter username@hostname",
-                                         System.getProperty("user.name")+
-                                         "@localhost"); 
-      }
-      String user=host.substring(0, host.indexOf('@'));
-      host=host.substring(host.indexOf('@')+1);
-
-      Session session=jsch.getSession(user, host, 22);
-
-      // username and password will be given via UserInfo interface.
-      UserInfo ui=new SwingDialogUserInfo();
-      session.setUserInfo(ui);
-
-      session.connect();
-
-      Channel channel=session.openChannel("shell");
-
-      channel.setInputStream(System.in);
-      channel.setOutputStream(System.out);
-
-      channel.connect();
+    String host=null;
+    if(arg.length>0){
+      host=arg[0];
     }
-    catch(Exception e){
-      System.out.println(e);
+    else{
+      host=JOptionPane.showInputDialog("Enter username@hostname",
+                                       System.getProperty("user.name")+
+                                       "@localhost"); 
     }
+    String user=host.substring(0, host.indexOf('@'));
+    host=host.substring(host.indexOf('@')+1);
+
+    Session session=jsch.getSession(user, host, 22);
+
+    // username and password will be given via UserInfo interface.
+    UserInfo ui=new SwingDialogUserInfo();
+    session.setUserInfo(ui);
+
+    session.connect();
+
+    Channel channel=session.openChannel("shell");
+
+    channel.setInputStream(System.in);
+    channel.setOutputStream(System.out);
+
+    channel.connect();
   }
 
+  /**
+   * A Logger implementation.
+   */
   public static class MyLogger implements com.jcraft.jsch.Logger {
     static java.util.Hashtable name=new java.util.Hashtable();
     static{
@@ -61,5 +68,3 @@ public class Logger{
   }
 
 }
-
-
