64a4bf5d9f0746f3f462b803b2a68976127a80e9
Paŭlo Ebermann <paul@fencing-game.de>
2011-05-08 15:50:02 +0200

    ChangePassphrase: comments, exit(1) on error.

--- examples/ChangePassphrase.java
+++ examples/ChangePassphrase.java
@@ -2,6 +2,15 @@
 import com.jcraft.jsch.*;
 import javax.swing.*;
 
+/**
+ * This program will demonstrate to change the passphrase for a
+ * private key file instead of creating a new private key.
+ *
+ * A passphrase will be prompted if the given private-key has been
+ * encrypted.  After successfully loading the content of the
+ * private-key, the new passphrase will be prompted and the given
+ * private-key will be re-encrypted with that new passphrase.
+ */
 class ChangePassphrase{
   public static void main(String[] arg){
     if(arg.length!=1){
@@ -20,21 +29,22 @@ class ChangePassphrase{
 
       String passphrase="";
       while(kpair.isEncrypted()){
-	JTextField passphraseField=(JTextField)new JPasswordField(20);
-	Object[] ob={passphraseField};
-	int result=JOptionPane.showConfirmDialog(null, ob, 
-						 "Enter passphrase for "+pkey,
-						 JOptionPane.OK_CANCEL_OPTION);
-	if(result!=JOptionPane.OK_OPTION){
-	  System.exit(-1);
-	}
-	passphrase=passphraseField.getText();
-	if(!kpair.decrypt(passphrase)){
-	  System.out.println("failed to decrypt "+pkey);
-	}
-	else{
-	  System.out.println(pkey+" is decrypted.");
-	}
+        JTextField passphraseField=(JTextField)new JPasswordField(20);
+        Object[] ob={passphraseField};
+        int result=JOptionPane.showConfirmDialog(null, ob, 
+                                                 "Enter passphrase for "+pkey,
+                                                 JOptionPane.OK_CANCEL_OPTION);
+        if(result!=JOptionPane.OK_OPTION){
+          System.exit(-1);
+        }
+        passphrase=passphraseField.getText();
+        if(!kpair.decrypt(passphrase)){
+          System.out.println("failed to decrypt "+pkey);
+        }
+        else{
+          System.out.println(pkey+" is decrypted.");
+          System.exit(1);
+        }
       }
 
       passphrase="";
@@ -42,21 +52,22 @@ class ChangePassphrase{
       JTextField passphraseField=(JTextField)new JPasswordField(20);
       Object[] ob={passphraseField};
       int result=JOptionPane.showConfirmDialog(null, ob, 
-					       "Enter new passphrase for "+pkey+
-					       " (empty for no passphrase)",
-					       JOptionPane.OK_CANCEL_OPTION);
+                                               "Enter new passphrase for "+pkey+
+                                               " (empty for no passphrase)",
+                                               JOptionPane.OK_CANCEL_OPTION);
       if(result!=JOptionPane.OK_OPTION){
-	System.exit(-1);
+        System.exit(-1);
       }
       passphrase=passphraseField.getText();
 
       kpair.setPassphrase(passphrase);
       kpair.writePrivateKey(pkey);
       kpair.dispose();
+      System.exit(0);
     }
     catch(Exception e){
       System.out.println(e);
+      System.exit(1);
     }
-    System.exit(0);
   }
 }
