63b5d4a9278735d190b1aadd66a1f6b271aa355d
Paŭlo Ebermann <paul@fencing-game.de>
2011-05-11 01:24:27 +0200

    X11Forwarding: works now by providing the cookie.

--- examples/X11Forwarding.java
+++ examples/X11Forwarding.java
@@ -5,61 +5,89 @@ import javax.swing.*;
 /**
  * This program will demonstrate X11 forwarding.
  *
- * You will be asked username, hostname, displayname and passwd. 
+ * You will be asked username, hostname, displayname,
+ * X-authentication cookie and passwd.
  * If your X server does not run at 127.0.0.1:0, please enter
  * correct displayname. You need your X server be reachable by
- * TCP, not only unix-sockets.
+ * TCP, not only unix-sockets or other local means.
  *
  * If everything works fine, you will get the shell prompt.
  * Try X applications; for example, xlogo.
+ *
+ * You can also supply arguments by command line:
+ *   user@host  cookie  display
  */
 public class X11Forwarding{
   public static void main(String[] arg)
     throws Exception
   {
+    JSch jsch=new JSch();
 
-    String xhost="127.0.0.1";
-    int xport=0;
+    // get the target host and open session.
 
-      JSch jsch=new JSch();  
+    String host=null;
+    if(arg.length>0){
+      host=arg[0];
+    }
+    else{
+      host=JOptionPane.showInputDialog("Enter username@hostname",
+                                       System.getProperty("user.name")+
+                                       "@localhost");
+    }
+    String user=host.substring(0, host.indexOf('@'));
+    host=host.substring(host.indexOf('@')+1);
 
-      String host=null;
-      if(arg.length>0){
-        host=arg[0];
+    Session session=jsch.getSession(user, host, 22);
+
+
+    // get the right display
+
+    String display;
+    if( arg.length > 2) {
+      display = arg[2];
+    }
+    else {
+      display = System.getenv("DISPLAY");
+      if(display == null) {
+        display = ":0";
       }
-      else{
-        host=JOptionPane.showInputDialog("Enter username@hostname",
-                                         System.getProperty("user.name")+
-                                         "@localhost"); 
+      if(display.startsWith(":")) {
+        display = "localhost" + display;
       }
-      String user=host.substring(0, host.indexOf('@'));
-      host=host.substring(host.indexOf('@')+1);
+      // ask the user:
+      display=
+        JOptionPane.showInputDialog("Please enter display name", display);
+    }
+    String xhost=display.substring(0, display.indexOf(':'));
+    if(xhost.equals("")) {
+      xhost = "localhost";
+    }
+    int xport=Integer.parseInt(display.substring(display.indexOf(':')+1));
 
-      Session session=jsch.getSession(user, host, 22);
+    session.setX11Host(xhost);
+    session.setX11Port(xport+6000);
 
-      String display=JOptionPane.showInputDialog("Please enter display name", 
-						 xhost+":"+xport);
-      xhost=display.substring(0, display.indexOf(':'));
-      xport=Integer.parseInt(display.substring(display.indexOf(':')+1));
+    // setup the X cookie.
 
-      session.setX11Host(xhost);
-      session.setX11Port(xport+6000);
+    String cookie =
+      arg.length > 1 ? arg[1] :
+      JOptionPane.showInputDialog("please enter cookie for display\n" +
+                                  "(try \"xauth list "+display+"\")");
+    session.setX11Cookie(cookie);
 
-      // username and password will be given via UserInfo interface.
-      UserInfo ui=new SwingDialogUserInfo();
-      session.setUserInfo(ui);
-      session.connect();
+    // username and password will be given via UserInfo interface.
+    UserInfo ui=new SwingDialogUserInfo();
+    session.setUserInfo(ui);
+    session.connect();
 
-      Channel channel=session.openChannel("shell");
+    Channel channel=session.openChannel("shell");
 
-      channel.setXForwarding(true);
+    channel.setXForwarding(true);
 
-      channel.setInputStream(System.in);
-      channel.setOutputStream(System.out);
+    channel.setInputStream(System.in);
+    channel.setOutputStream(System.out);
 
-      channel.connect();
+    channel.connect();
   }
 
 }
-
-
